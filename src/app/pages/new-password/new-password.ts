fcc7227461dc976fdcfbc7aae6fe0f8612dfd49f
IgorToledoOFC <igorsilvatoledoofc@gmail.com>
2026-05-14 12:37:39 -0300

    :card_file_box: Added new-password, forgot-password components

---
+++ src/app/pages/new-password/new-password.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-new-password',
+  imports: [RouterOutlet],
+  templateUrl: './new-password.html',
+  styleUrl: './new-password.css',
+})
+export class NewPassword {}
