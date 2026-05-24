9d8747cda39fabb96114a03d8f31266dbe684576
IgorToledoOFC <igorsilvatoledoofc@gmail.com>
2026-05-24 00:04:59 -0300

    feat: :art: Home stetic implemented Need: Test and add functionaties

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ export const routes: Routes = [
 
   {path: '',loadComponent: () => import('./pages/home/home').then(m => m.Home)},
 
-  {path: 'login',loadComponent: () =>import('./pages/login/login').then(m => m.Login),
-    children: [{path: 'forgot-password',loadComponent: () =>import('./pages/forgot-password/forgot-password').then(m => m.ForgotPassword),
-    children: [{path: 'new-password',loadComponent: () =>import('./pages/new-password/new-password').then(m => m.NewPassword)}]}]
-  },
+  {path: 'login',loadComponent: () =>import('./pages/login/login').then(m => m.Login)},
+
   {path: 'register',loadComponent: () =>import('./pages/register/register').then(m => m.Register)},
+
+  {path: 'forgot-password',loadComponent: () =>import('./pages/forgot-password/forgot-password').then(m => m.ForgotPassword)},
+
+  {path: 'new-password',loadComponent: () =>import('./pages/new-password/new-password').then(m => m.NewPassword)},
 
 
   {path: '**',redirectTo: ''}
