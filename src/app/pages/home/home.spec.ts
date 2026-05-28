4cf890195e125dc3a3a768405c7681bd386d8fe7
IgorToledoOFC <igorsilvatoledoofc@gmail.com>
2026-05-28 11:59:46 -0300

    test: :test_tube: Added tests Buttons to Home.  Alterated: home.html to test work

--- src/app/pages/home/home.spec.ts
+++ src/app/pages/home/home.spec.ts
@@ -1,20 +1,71 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Router } from '@angular/router';
 import { Home } from './home';
+import { By } from '@angular/platform-browser';
 
 describe('Home', () => {
+
   let component: Home;
   let fixture: ComponentFixture<Home>;
+  let router: Router;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({imports: [Home],providers: [provideRouter([])]}).compileComponents();
+
+    await TestBed.configureTestingModule({
+      imports: [Home],
+      providers: [provideRouter([])]
+    }).compileComponents();
+
+    router = TestBed.inject(Router);
 
     fixture = TestBed.createComponent(Home);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  function getButton(selector: string): HTMLButtonElement {
+    const button = fixture.debugElement.query(By.css(selector));
+
+    expect(button).not.toBeNull();
+
+    return button.nativeElement;
+  }
+
+  describe('Navigation Buttons', () => {
+
+    it('should navigate to login page', () => {
+
+      const navigateSpy = spyOn(router, 'navigateByUrl');
+
+      getButton('.primary').click();
+
+      expect(navigateSpy).toHaveBeenCalled();
+    });
+
+    it('should navigate to register page', () => {
+
+      const navigateSpy = spyOn(router, 'navigateByUrl');
+
+      getButton('.secondary').click();
+
+      expect(navigateSpy).toHaveBeenCalled();
+    });
+
   });
+
+  describe('Support Button', () => {
+
+    it('should open support email', () => {
+
+      const openSpy = spyOn(window, 'open');
+
+      getButton('.support').click();
+
+      expect(openSpy).toHaveBeenCalled();
+
+    });
+
+  });
+
 });
