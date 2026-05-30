90325fe7fcb93452708e898b3ca4c212b4206672
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-05-30 17:15:43 -0300

    feat: ✨ new resource, consuming api

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { UserService } from '../../core/UserService/userService';
+import { userInterface } from '../../core/UserService/userInterface';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './register.html',
   styleUrls: ['./register.css'],
 })
@@ -15,4 +18,26 @@ export class Register {
   toggleSenha() {
     this.mostrarSenha = !this.mostrarSenha;
   }
+
+  constructor(private userService: UserService) {}
+
+  user: userInterface = {
+    email: '',
+    password: '',
+    name: ''
+  }
+
+  save(): void {
+   this.userService
+    .saveUser(this.user)
+    .subscribe({
+    next: (response) => {
+      console.log('User saved with success:');
+      console.log(response);
+    },
+    error: (error) => {
+      console.error('Error saving user:', error.message);
+    }
+  });
+}
 }
