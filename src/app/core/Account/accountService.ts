434e4dba75632d84ef06e6e0a42576dd71ce4f88
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-06-01 14:39:41 -0300

    feat: ✨ creting nre resource, refresh in accounts monitored and button go to out, added also log to show log of browser and a sleep to wait backend finish to show in interface

---
+++ src/app/core/Account/accountService.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AccountInterface } from './accountInterface';
+import { API_PATH } from '../../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class AccountService {
+
+  constructor(private http: HttpClient) {}
+
+  getByUserId(userId: number): Observable<AccountInterface[]> {
+    return this.http.get<AccountInterface[]>(`${API_PATH}/accounts/user/${userId}`);
+  }
+
+addMonitored(userId: number, email: string): Observable<string> {
+  const encodedEmail = encodeURIComponent(email);
+  return this.http.post(
+    `${API_PATH}/accounts/accountMonitored/${userId}/${encodedEmail}`,
+    {},
+    { responseType: 'text' }
+  ) as Observable<string>;
+}
+
+  deleteById(id: number): Observable<void> {
+    return this.http.delete<void>(`${API_PATH}/accounts/${id}`);
+  }
+
+  refreshEmail(userId: number, email: string): Observable<string> {
+  const encodedEmail = encodeURIComponent(email);
+  return this.http.put(
+    `${API_PATH}/accounts/refresh/${userId}/${encodedEmail}`,
+    {},
+    { responseType: 'text' }
+  ) as Observable<string>;
+}
+}
